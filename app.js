18ef3f9bea74ec6496a6fd09c11ad30c746e1d57
Ashmit <ashmitforwork@gmail.com>
2023-06-03 18:12:59 +0530

    Testing VS Commit

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const connectDB = require('./db/connect')
 
 const notFoundMiddleware = require('./middleware/not-found')
 const errorMiddleware = require('./middleware/error-handler')
-
+const {getAllProducts, getAllProductsStatic} = require("./controllers/products");
+const productsRouter = require('./routes/products')
 // middleware
 app.use(express.json())
 
@@ -15,6 +16,8 @@ app.use(express.json())
 app.get('/', (req, res)=>{
     res.send('<h1>Store API</h1><a href="/api/v1/products">Products Route</a>')
 })
+
+app.use('/api/v1/products', productsRouter)
 
 // products route
 
